0630a3efe2a7b6d7f4e2483ad328df0e36b8287d
Andrew <andrewgardner@live.com>
2019-02-25 13:01:13 -0700

    added the rest to linearTester

--- DataStructures/Testers/LinearTester.cpp
+++ DataStructures/Testers/LinearTester.cpp
@@ -1,5 +1,5 @@
 //
-//  LinearTester.cpp
+//  LinearTester.hpp
 //  DataStructures
 //
 //  Created by Gardner, Andrew on 2/13/19.
@@ -40,5 +40,36 @@ void LinearTester :: testVsSTL()
     crimeTimerSTL.resetTimer();
     musicOOP.resetTimer();
     musicSTL.resetTimer();
-                                                                       
+    
+    int randomIndex = rand() % moreCrimes.getSize();
+    
+    crimeTimerSTL.startTimer();
+    crimes[randomIndex];
+    crimeTimerSTL.stopTimer();
+    
+    crimeTimerOOP.startTimer();
+    moreCrimes.getFromIndex(randomIndex);
+    crimeTimerOOP.stopTimer();
+    
+    cout << "This is the STL random retrieval: " << endl;
+    crimeTimerSTL.displayInformation();
+    cout << "This is the OOP Node random retrieval: " << endl;
+    crimeTimerOOP.displayInformation();
+    cout << "A difference of: " << crimeTimerOOP.getTimeInMicroseconds() - crimeTimerSTL.getTimeInMicroseconds() << " microseconds" << endl;
+    
+    randomIndex = rand() % musicList.getSize();
+    
+    musicSTL.startTimer();
+    tunez[randomIndex];
+    musicSTL.stopTimer();
+    
+    musicOOP.startTimer();
+    musicList.getFromIndex(randomIndex);
+    musicOOP.stopTimer();
+    
+    cout << "This is the STL random retrieval: " << endl;
+    musicSTL.displayInformation();
+    cout << "This is the OOP Node random retrieval: " << endl;
+    musicOOP.displayInformation();
+    cout << "A difference of: " << musicOOP.getTimeInMicroseconds() - musicSTL.getTimeInMicroseconds() << " microseconds" << endl;
 }
